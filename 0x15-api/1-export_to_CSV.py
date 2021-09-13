ae10ae5ae15ca60e4c3cbdd4283ca5728e5f5902
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-13 20:43:22 -0300

    upload file please pep8 I beg

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" extend past Python script to export data in the CSV format."""
+import requests
+import csv
+from sys import argv
+
+from requests import models
+
+
+if __name__ == '__main__':
+    ID = argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(ID)
+    todoList = requests.get(url)
+    url = "https://jsonplaceholder.typicode.com/users?id={}".format(ID)
+    user = requests.get(url)
+    tL = todoList.json()
+    u = user.json()[0]
+    name = u.get('name')
+    file = "{}.csv".format(ID)
+    with open(file, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in tL:
+            status = task.get('completed')
+            title = task.get('title')
+            writer.writerow([ID, name, status, title])
