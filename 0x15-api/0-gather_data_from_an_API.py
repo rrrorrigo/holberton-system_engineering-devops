4a55966937e61c680ad68802c352d512b2997b60
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-13 18:21:49 -0300

    upload file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress."""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+        ID = argv[1]
+        url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(ID)
+        todoList = requests.get(url)
+        url = "https://jsonplaceholder.typicode.com/users?id={}".format(ID)
+        user = requests.get(url)
+        tL = todoList.json()
+        u = user.json()[0]
+        name = u.get('name')
+        taskComplete = 0
+        for tC in tL:
+                taskComplete += 1 if tC.get('completed') else 0
+        string = "Employee {} is done with tasks({}/20)".format(name, taskComplete) 
+        print(string)
+        for task in tL:
+                if task.get('completed'):
+                        print("\t {}".format(task.get('title')))
