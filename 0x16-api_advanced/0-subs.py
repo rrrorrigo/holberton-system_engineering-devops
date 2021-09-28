382b10edf46f8abf2222b9719ef78713f72f9d6e
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-28 16:48:59 -0300

    upload file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,12 @@
 """function that queries the Reddit API and returns the number of subscribers
 If an invalid subreddit is given, the function should return 0"""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API and returns the number of
     subscribers. If an invalid subreddit is given,
     the function should return 0"""
-    subreddit = argv[1]
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={'User-agent': 'MyScript'})
     data = response.json()
