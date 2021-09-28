03cc92d960abeecd5c83c6cdec99e812fcdc51d9
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-28 17:58:39 -0300

    fixedd

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers={'User-agent': 'MyScript'})
     data = response.json()
     data = data.get('data').get('children')
@@ -15,5 +15,4 @@ def top_ten(subreddit):
         print("None")
         return
     for i in range(10):
-        if i < 10:
-            print(data[i].get('data').get('title'))
+        print(data[i].get('data').get('title'))
