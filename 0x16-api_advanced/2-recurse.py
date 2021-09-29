47c1644fea8b12c94410938f867bbb9a1376a19e
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-29 15:24:40 -0300

    I'm afraid to test it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,10 +11,10 @@ def recurse(subreddit, hot_list=[], after=""):
                       allow_redirects=False)
     if req.status_code != 200:
         return None
-    r = req.json()['data']['children']
-    for title in req.get('data').get('children'):
+    r = req.json()
+    for title in r.get('data').get('children'):
         hot_list.append(title.get('data').get('title'))
-    afterId = req.get('data').get('after')
+    afterId = r.get('data').get('after')
     if afterId is not None:
         recurse(subreddit, hot_list, afterId)
     return hot_list
